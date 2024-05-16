64f085b708995d53972d64e3456f850b0212dc20
Miras <91594617+laqube@users.noreply.github.com>
2024-05-17 03:03:33 +0500

    added section 1
    -added section "about us"
    -moved media assets to the public folder because I'm using vite
    -fixed navbar stylings

--- src/components/sections/Header/Header.jsx
+++ src/components/sections/Header/Header.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import Locales from "../../ui/Locales/LocalesDrop";
 import { useLanguage } from "../../../LanguageContext";
-import "./header.css";
+import styles from "./header.module.css";
 import Navigation from "../../Navigation/Navigation";
 
 const Header = () => {
   const { language } = useLanguage();
   return (
-    <div className="header">
-      <div className="nav">
+    <div className={styles.header}>
+      <div className={styles.nav}>
         <a
           href={language === "kz" ? `/` : `/${language}`}
-          className="header__logo"
+          className={styles.header__logo}
         >
-          <img src="src\assets\logo.png" alt="logo" />
+          <img src="assets/logo.png" alt="logo" />
         </a>
         <Navigation />
         <Locales />
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-          <img src="src\assets\profile.svg" alt="icon" />
+          <img src="assets/profile.svg" alt="icon" />
         </a>
       </div>
     </div>
--- src/components/sections/Mission/Mission.jsx
+++ src/components/sections/Mission/Mission.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import styles from "./mission.module.css";
 
 const Mission = () => {
-  return <div>Mission</div>;
+  const { t } = useTranslation("translation");
+  return (
+    <div className={styles.section}>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.h1}>{t("main.title")}</div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.p}>{t("main.article")}</div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.card}>
+              <div className={styles.card_image}>
+                <img src="/assets/mission_icon1.png" alt="icon1" />
+              </div>
+              <div className={styles.card_number}>
+                {t("main.elements.0.number")}
+              </div>
+              <div className={styles.card_word}>
+                {t("main.elements.0.word")}
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.card_image}>
+                <img src="/assets/mission_icon2.png" alt="icon1" />
+              </div>
+              <div className={styles.card_number}>
+                {t("main.elements.1.number")}
+              </div>
+              <div className={styles.card_word}>
+                {t("main.elements.1.word")}
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.card_image}>
+                <img src="/assets/mission_icon3.png" alt="icon1" />
+              </div>
+              <div className={styles.card_number}>
+                {t("main.elements.2.number")}
+              </div>
+              <div className={styles.card_word}>
+                {t("main.elements.2.word")}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.image_container}>
+          <img
+            src="/assets/mission_banner.png"
+            alt="banner"
+            className={styles.image}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mission;
